648dd475de3709f2791271b34856c5911fd7b617
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-20 22:48:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@
      return "I like to help people."
    }
 
-   this.releasePressStatement = function(){
+   BoardMember.prototype.releasePressStatement = function(){
      return "You will see great things from Scuber."
    }
 
-   this.sayHi = function(){
+   BoardMember.prototype.sayHi = function(){
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
