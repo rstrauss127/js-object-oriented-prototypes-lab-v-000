d4bcbfb78c6c654019acfd2c3a32b8c2e7154777
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-20 22:45:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,15 +9,15 @@
   this.approve = function(){
     return 'You can do that!'
    }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+   this.doCharity = function(){
+     return "I like to help people."
+   }
+
+   this.releasePressStatement = function(){
+     return "You will see great things from Scuber."
+   }
+
+   this.sayHi = function(){
+     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+   }
+ }
